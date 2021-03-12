3c1c4c4c4e6fb80584809d1c089f098acff01d31
Natechawin <natechawin@gmail.com>
2021-03-12 18:10:49 +0700

    Add readfile and print content

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let to_find = &args[1];
+    let filename = &args[2];
+
+    println!("Search for {}", to_find);
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Cannot read file.");
+
+    println!("Content is {}", contents);
 }
