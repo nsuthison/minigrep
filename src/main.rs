5ef99ce70a9bb1afb63c0c0eb35de657bf4137d4
Natechawin <natechawin@gmail.com>
2021-03-14 23:48:17 +0700

    Changenew config error handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = match Config::new(&args) {
+        Ok(value) => value,
+        Err(error) => {
+            println!("{}", String::from(error));
+            process::exit(1);
+        }
+    };
 
     println!("Search for {}", config.to_search);
     println!("In file {}", config.filename);
@@ -20,10 +27,16 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
-        Config {
-            to_search: args[1].clone(),
-            filename: args[2].clone(),
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
         }
+
+        Ok(
+            Config {
+                to_search: args[1].clone(),
+                filename: args[2].clone(),
+            }
+        )
     }
 }
