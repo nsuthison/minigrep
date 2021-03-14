3eb7f42da75047103775433d73dc994cec115402
Natechawin <natechawin@gmail.com>
2021-03-14 23:39:28 +0700

    Move create config to Config ctor to make it more concise

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Search for {}", config.to_search);
     println!("In file {}", config.filename);
@@ -14,14 +14,16 @@ fn main() {
     println!("Content is {}", contents);
 }
 
-fn parse_config(args: &[String]) -> Config {
-    Config {
-        to_search: args[1].clone(),
-        filename: args[2].clone(),
-    }
-}
-
 struct Config {
     to_search: String,
     filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        Config {
+            to_search: args[1].clone(),
+            filename: args[2].clone(),
+        }
+    }
 }
